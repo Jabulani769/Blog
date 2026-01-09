a2282a086074b5ea7fbafec83c07e40d39d542d4
Jabulani769 <jabulanikapherachoka@gmail.com>
2026-01-09 22:19:53 +0200

    the forgotten

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
-
-export default defineConfig({
-  site: 'https://jkblog.vercel.app',
-  vite: {
-    plugins: [tailwindcss()]
-  }
-});
+// @ts-check                                  │
+import { defineConfig } from 'astro/config';                                          
+import tailwindcss from '@tailwindcss/vite';                                       
+import sitemapIntegration from '@astrojs/sitemap';     
+                                            
+export default defineConfig({               
+  integrations: [sitemapIntegration()],                
+  site: 'https://jkblog.vercel.app',        
+  vite: {                                   
+    plugins: [tailwindcss()]                
+  }                                         
+});                                         
+                                            
+function sitemap() {                        
+  return {                                  
+    name: 'sitemap',                        
+    hooks: {}                               
+  };                                        
+} 
